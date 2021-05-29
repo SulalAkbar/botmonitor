214b925abb355488f719eac92d48ed55ed0118bd
sulal <sulalakbar5@gmail.com>
2021-05-29 08:00:45 +0500

    Changed Web Hook

--- controller.py
+++ controller.py
@@ -14,7 +14,7 @@ INTERVAL_TIME = 5
 
 #Enter URL of channel for which you want to recive notifications
 #https://discord.com/api/webhooks/843695641598885918/d3VuT_VZ6EMxxMmRwnVOvu2YxyMNPYWALgsb9soXzkz-lc55cDhLCbpGnJw9cYlMMEA_
-WEB_HOOK = "https://discord.com/api/webhooks/847318723349315625/zomyj_B_pMEO6iQiZaV9FRAachF-CfsryFWyS8d8ecTaDYHlKUTeNIoayo8-nW4cK2aX"
+WEB_HOOK = "https://discord.com/api/webhooks/843695641598885918/d3VuT_VZ6EMxxMmRwnVOvu2YxyMNPYWALgsb9soXzkz-lc55cDhLCbpGnJw9cYlMMEA_"
 
 
 
